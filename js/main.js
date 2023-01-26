2246e45f3ea95933c2a6daeb9435efa0ad144e03
Rafika Hutami Putri <rfkahtmi23@gmail.com>
2023-01-26 15:32:01 +0800

    finish

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 // TOGGLE MENU
-
 const navToggle = document.getElementById("nav-toggle"),
     navMenu = document.getElementById("nav-menu");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("top-[-1000%]");
-    navMenu.classList.toggle("top-14");
+    navMenu.classList.toggle("top-20");
+});
+
+// ADD SHADOW TO HEADER WHEN SCROLLING
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", function () {
+    const scrollY = this.scrollY;
+
+    header.classList[scrollY > 20 ? "add" : "remove"]("shadow-sm");
 });
